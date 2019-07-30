243fa2561d805994a7c70ab6ceedb9f22f83ab53
benevolent-demo-1612 <tevinsherman@yahoo.com>
2019-07-30 00:45:14 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,21 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  find_element_index = [2,3,4,5]
+  find_element_index.index(0)
+  find_element_index.index(6)
 end
 
 def find_max_value(array)
   # Add your solution here
+  find_max_value = [2,3,4,5]
+   find_max_value.max(1)
 end
+  
 
 def find_min_value(array)
   # Add your solution here
 end
+
+#a = %w(albatross dog horse)
+#a.max                                   #=> "horse"
+#a.max { |a, b| a.length <=> b.length }  #=> "albatross"
